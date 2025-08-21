967118c1574047ab7f4cf1cd95ffba47607067b1
pabloavalos25 <pablo.avalos192@hmail.com>
2025-08-21 09:05:04 -0300

    fff

---
+++ Ejercicio.py
@@ -0,0 +1,52 @@
+numero1= 20
+numero2= 2.4
+suma=numero1+numero2
+print(suma)
+
+resta=numero1-numero2
+print(resta)
+
+multiplicacion=numero1*numero2
+print(multiplicacion)
+
+division=numero1/numero2
+print(division)
+
+nombre="Pablo"
+precio=10.50
+descuento=0.20
+
+precio_final= precio - (10.50 * 0.20 / 100)
+print("El precio final es", precio_final)
+
+cadena="tostadora"
+longitud=len(cadena)
+print(longitud)
+
+precio_dec= 20.50
+precio_ent=int(precio_dec)
+print(precio_dec)
+print(precio_ent)
+
+nombre="Pablo"
+apellido="Avalos"
+nombre_completo= nombre +  apellido#
+print(nombre_completo)
+
+edad=25
+multi=edad*5
+print(multi)
+dismi=multi/10
+print(dismi)
+
+altura=1.75
+mul=(altura*4)
+print(mul)
+divi=(altura/3)
+print(divi)
+
+texto="PABLO"
+texto_minuscula=texto.lower()
+print(texto_minuscula)
+
+print(texto.capitalize())
