87477ae60923a9cb97bcb1e519b282d040620144
pabloavalos25 <pablo.avalos192@hmail.com>
2025-09-18 16:13:56 -0300

    TP5_LISTAS

---
+++ TRABAJOS PRÁCTICOS/TP 5 LISTAS/LISTAS.py
@@ -0,0 +1,78 @@
+#Ejercicio_1
+
+Lista_eje1_multi4=list(range(4,101,4))
+
+print(Lista_eje1_multi4)
+
+#Ejercicio_2
+
+lista_objetos=["consola","Hamburguesas","Netflix","Soda","Simulador"]
+
+print(lista_objetos[4])
+
+#Ejericio_3
+
+lista_vacia=[]
+
+lista_vacia.append("Simulador_de_carreras")
+lista_vacia.append("Soda")
+lista_vacia.append("Gato")
+
+print(lista_vacia)
+
+
+#Ejercicio_4
+
+lista_eje4=["perro","gato","conejo","pez"]
+
+lista_eje4[3] = "Loro"
+lista_eje4[3] = "Oso"
+
+print(lista_eje4)
+
+#Ejercicio_5
+
+#Elimina el valor mas alto de la lista, en el caso del ejemplo seria el número 22
+
+#Ejercicio_6
+
+lista_numeros=list(range(10,30+1,5))
+
+print(lista_numeros)
+
+#Ejericio_7
+
+autos=["Sedan","polo","suran","gol"]
+
+autos[1] = "Mclaren"
+autos[2] = "Deportivo"
+
+print(autos)
+
+#Ejercicio_8
+
+dobles=[]
+
+dobles.append("10")
+dobles.append("20")
+dobles.append("30")
+
+print(dobles)
+
+#Ejercicio_9
+
+compras=[["pan","leche"], ["arroz","fideos","salsa"],["agua"]]
+
+compras[2].append("jugo")
+
+compras[1][1] = "tallarines"
+
+compras[0].remove("pan")
+
+print(compras)
+
+#Ejercicio_10
+
+lista_anidada=[15,True,[25.5,57.9,30.6],False]
+
+print(lista_anidada)
